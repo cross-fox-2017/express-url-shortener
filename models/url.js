ca69fc1004e8e29397f7340002f174b73dc7af4d
Syanmil <syanmil@yahoo.com>
2017-01-25 12:58:25 +0700

    link redirect and clickcount done

--- models/url.js
+++ models/url.js
@@ -4,6 +4,13 @@ module.exports = function(sequelize, DataTypes) {
     url: DataTypes.STRING,
     shorturl: DataTypes.STRING,
     clickcount: DataTypes.INTEGER
+  },{
+    hooks: {
+      beforeCreate: function(Url, options) {
+        Url.shorturl = random(),
+        Url.clickcount = 0
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
@@ -13,3 +20,20 @@ module.exports = function(sequelize, DataTypes) {
   });
   return Url;
 };
+
+function random() {
+  let result = []
+  for (let i = 0; i < 8; i++){
+    if (i%4 == 0) {
+      let randA = String.fromCharCode(Math.floor(Math.random()*(90-65)+65))
+      result.push(randA)
+    } else if (i%3 == 0){
+      let randB = String.fromCharCode(Math.floor(Math.random()*(122-97)+97))
+      result.push(randB)
+    } else {
+      let randC = Math.floor(Math.random()*9)
+      result.push(randC)
+    }
+  }
+  return result.join("")
+}
--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,26 @@ var url = models.Url
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  url.findAll().then(function(data){
+  url.findAll({order: [['id', 'ASC']]}).then(function(data){
     res.render('index', { title: 'Express', data: data });
   })
 });
 
 router.post('/urls', function(req, res, next) {
-  res.redirect('/')
+  let link = req.body.link
+  url.create({url: link}).then(function() {
+    res.redirect('/')
+  })
 });
 
 router.get('/:short_url', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let short = req.params.short_url
+  url.find({where: {shorturl : short}}).then(function (data){
+    let plus = data.clickcount + 1
+    data.update({clickcount: plus}).then(function() {
+      res.redirect(data.url)
+    })
+  })
 });
 
 module.exports = router;
