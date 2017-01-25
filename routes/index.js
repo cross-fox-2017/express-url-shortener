286d69bae3b2ad6865d463a2596d5a3a46dc5345
Irwin Pratajaya & Mangku Widodo <pair+irwinpratajaya.mangkuwi@hacktiv8.com>
2017-01-25 20:04:06 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const models = require('../models');
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   models.Url.findAll({raw: true}).then(function (data) {
-    res.render('index',{title: 'Shortener URL ', link: data})
+    res.render('index',{title: 'Shortener URL ', link: data, config: config.base_url})
   })
 });
 
@@ -22,7 +24,7 @@ router.get('/:short_url', function(req, res, next) {
   models.Url.findOne({
     where: {short_url: req.params.short_url}
   }).then(function(data) {
-      data.updateAttributes({
+      data.update({
          click_count : data.click_count + 1
       })
     res.redirect(`${data.url_link}`)
